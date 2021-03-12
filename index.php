29750c0199428f77ea2f24a7f97f21020fc5519d
Diego Alonso Ojeda Medina <dojedam@sena.edu.co>
2021-03-12 07:58:01 -0500

    Menu de Navegacion

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mi Primera Pagina</title>
+</head>
+<body style="background: gray">
+    <h1>Principal</h1>
+    <div>
+        <ol>
+            <li><a href="index.php"><strong>Principal</strong></a> </li>
+            <li><a href="index2.php">Acerca de</a></li>
+            <li><a href="index3.php">Contactenos</a></li>
+        </ol>
+    </div>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mi Primera Pagina</title>
+</head>
+<body style="background: dodgerblue">
+<h1>Acerca de</h1>
+<div>
+    <ul>
+        <li><a href="index.php">Principal</a> </li>
+        <li><a href="index2.php"><strong>Acerca de</strong></a></li>
+        <li><a href="index3.php">Contactenos</a></li>
+    </ul>
+</div>
+</body>
+</html>
